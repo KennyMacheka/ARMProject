8a0cb339e3e83caaaec7275d068ea5acc145805a
wz3117 <wz3117@imperial.ac.uk>
2018-05-25 15:06:09 +0100

    progress on implementation of dp ins function

--- src/assemble.c
+++ src/assemble.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <stdint.h>
 #include <string.h>
 
 /*
@@ -61,26 +62,52 @@ int convert_to_int(char *binary) {
 }
 /* Input is a string start with '#', might be decimal(#nnn) or hexadecimal(#0xnnn)
    Output is a string represents the corresponding 12-bit binary number which first 4 bits are in
+/* Turn a string start with '#', might be decimal(#nnn) or hexadecimal(#0xnnn)
+   into a string represents the corresponding 12-bit binary number which first 4 bits are in
    rotate field and last 8 bits are numbers.
    This 12-bit number actually represents a 32-bit number which is got by represent the 8-bit number
    in 32-bit format and filling 0 in higher bits, then rotate right the 32-bit number by (4-bit number * 2)
 */
 //Q: e.g. how to represent #0x1999? Just error? however is within memory address limit
-char* immToBinary(char* imm) {
-  char result[13] = "";
-  //TODO
-  return result;
+void immToBinary(char imm[], char result[]) {
+//TODO
+
+
 }
+
 /* When Operand2 is a shift register, deal with it another way (optional task) */
 
-/* Input is a string start with 'r' followed by a decimal number
-   Output is a string represents the number in 4-bit binary
+/* Turn a string start with 'r' followed by a decimal number
+   into a string represents the number in 4-bit binary format
 */
-char* regNumToBinary(char* regNum) {
-  char result[5] = "";
-  //TODO
-  return result;
+void regNumToBinary(char regNum[],char result[]) {
+  assert(regNum[0] == 'r');
+  uint16_t num = 0;
+  if(regNum[2]){ //when for Rn, n>=10
+    num = 10;
+    num += ((int)regNum[2] - (int)'0');
+  } else {
+    num += ((int)regNum[1] - (int)'0');
+  }
+  uint16_t mask = 1 << 3;
+  for(int i = 0; i < 4; i++) {
+    if((num & mask) == 0) {
+      result[i] = '0';
+    } else {
+      result[i] = '1';
+    }
+    num = num << 1;
+  }
 }
+
+/* take in a token, if is immediate, return 1; else return 0
+ * */
+int isImm(char token[]){
+  if(token[0] == '#')
+    return 1;
+  return 0;
+}
+
 int convert_to_int(char *binary) {
   int ints[sizeof(binary)/ 32];
 
@@ -98,12 +125,43 @@ int convert_to_int(char *binary) {
 /* Input is a tokenised instruction with instruction number given(see doc/instructionNum)
  * Output is a string contains '0' and '1' represents required binary number
  * */
-char* data_process_ins_assembler(char** tokenised_ins, int insNum) {
-  char result[33] = "111000";
+//TODO:remember to free
+//TODO:把相似部分拿出来，以优化switch,减少重复
+char* data_process_ins_assembler(char tokenised_ins[][12], int tokenCount, int insNum) {
+  char *result = calloc(33, sizeof(char));
+  result[0] = '1';//cond
+  result[1] = '1';
+  result[2] = '1';
+  result[3] = '0';
+  result[4] = '0';//00
+  result[5] = '0';
   switch (insNum) {
     case 3://and
-			//assert argument no. is 4
-		  //check if tokenised_ins[3] is imm
+      assert(tokenCount == 4);
+		  result[6] = (char) ((int) '0' + isImm(tokenised_ins[3])); //I
+      result[7] = '0';//Opcodes
+      result[8] = '0';
+      result[9] = '0';
+      result[10] = '0';
+      result[11] = '0'; //S
+      char biRnNo[5] = "";
+      regNumToBinary(tokenised_ins[2], biRnNo);
+      result[12] = biRnNo[0];//Rn
+      result[13] = biRnNo[1];
+      result[14] = biRnNo[2];
+      result[15] = biRnNo[3];
+      char biRdNo[5] = "";
+      regNumToBinary(tokenised_ins[1], biRdNo);
+      result[16] = biRdNo[0];//Rd
+      result[17] = biRdNo[1];
+      result[18] = biRdNo[2];
+      result[19] = biRdNo[3];
+      char biOp2No[13] = "";
+      if(result[6]) {
+        immToBinary(tokenised_ins[3], biOp2No);
+
+      } //else shifted register(optional)
+
 
     case 4://eor
     case 1://sub
