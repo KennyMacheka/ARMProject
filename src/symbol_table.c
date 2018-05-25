b3c82355b8a459cefbf4f75910d555a67f2da876
dhru.devalia17@imperial.ac.uk <dhru.devalia17@imperial.ac.uk>
2018-05-25 14:09:05 +0100

    initial symbol table implementation

---
+++ src/symbol_table.c
@@ -0,0 +1,32 @@
+//
+// Created by Dhru on 25/05/2018.
+//
+
+#include <cstring>
+
+struct symbol_table{
+    int address[];
+    char *label[];
+    int size = 0;
+};
+
+void insert(struct symbol_table t, int address, char *label){
+  t.address[t.size] = address;
+  t.label[t.size] = label;
+  t.size++;
+}
+
+int get(struct symbol_table t, char *label){
+  for(int i = 0; i < t.size; i++){
+    if(strcmp(t.label[i], label)){
+      return t.address[i];
+    }
+  }
+  return -1;
+}
+
+
+
+
+
+
